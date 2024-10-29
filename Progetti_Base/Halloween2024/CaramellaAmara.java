415dbc391e28d6827932beb4be3fac33edb854c1
NIK_ <nicolas.marciello07@gmail.com>
2024-10-29 13:50:49 +0100

    cm

--- Progetti_Base/Halloween2024/CaramellaAmara.java
+++ Progetti_Base/Halloween2024/CaramellaAmara.java
@@ -8,9 +8,19 @@ public class CaramellaAmara extends Dolcetto {
         this.amarezza = amarezza;
     }
 
+    public void sputa(){
+        this.sputato = !sputato;
+    }
+
     @Override
     public void mangia(){
-        
+        if(amarezza <= 4){
+            System.out.println(nome + " è amarisima!");
+        }else if(amarezza > 4 && amarezza <= 8){
+            System.out.println(nome + " è neutra!");
+        }else{
+            System.out.println(nome + " è poco amara!");
+        }
     }
     
 }
