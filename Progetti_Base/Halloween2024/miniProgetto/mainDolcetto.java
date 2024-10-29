111a91669942a2d50080c417bcded31e0e7d5f8d
NIK_ <nicolas.marciello07@gmail.com>
2024-10-29 10:13:30 +0100

    tammazzo

--- Progetti_Base/Halloween2024/miniProgetto/mainDolcetto.java
+++ Progetti_Base/Halloween2024/miniProgetto/mainDolcetto.java
@@ -1,8 +1,8 @@
 public class mainDolcetto {
     public static void main(String[] args) {
-        String[] Lista = {"Cioccolato", "Latte", "Zucchero"};
-        Dolcetto biscottoClassico = new Dolcetto(5, 10, Lista);
-        Caramella Galatina = new Caramella(9, 10, Lista, 2);
+        String[] lista = {"Cioccolato", "Latte", "Zucchero"};
+        Dolcetto biscottoClassico = new Dolcetto(5, 10, lista);
+        Caramella Galatina = new Caramella(9, 10, lista, 2);
 
         biscottoClassico.mangia();
         biscottoClassico.print();
